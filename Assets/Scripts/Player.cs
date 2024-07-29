a92827bcedc440054c10b779ec4eb01844065cd5
Thiwanon Chomcharoen <tchomcharoen@zilo.co.uk>
2024-07-29 22:29:32 +0700

    :hammer: Refactoring Input System
    remove

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,33 +1,25 @@
 ﻿using UnityEngine;
 
 public class Player : MonoBehaviour {
+	[SerializeField] private GameInput gameInput;
+	
 	[SerializeField] private float moveSpeed = 7f;
 	[SerializeField] private float rotateSpeed = 10f;
 	
 	private bool isWalking; 
 	
 	private void Update() {
-	    Vector2 inputVector = new Vector2(0, 0);
+		Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 	    
-	    if (Input.GetKey(KeyCode.W)) {
-	    	inputVector.y = 1;
-	    }
-	    if (Input.GetKey(KeyCode.S)) {
-	    	inputVector.y = -1;
-	    }
-	    if (Input.GetKey(KeyCode.A)) {
-	    	inputVector.x = -1;
-	    }
-	    if (Input.GetKey(KeyCode.D)) {
-	    	inputVector.x = 1;
-	    }
-	    
-	    inputVector = inputVector.normalized;
-	    
-	    Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-		transform.position += moveDir * moveSpeed * Time.deltaTime;
+		Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
 	    
 		isWalking = moveDir != Vector3.zero;
+		
+		if (isWalking == false) {
+			return;
+		}
+	    
+		transform.position += moveDir * moveSpeed * Time.deltaTime;
 	    
 	    transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
 	}
